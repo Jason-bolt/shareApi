f5a6a5ca25fbeda87fe95d1b5fdd86af4430ff2c
Jason-bolt <appiatujason@gmail.com>
2022-12-02 12:14:36 +0000

    Tested API end point

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ const connectDB = require('./config/db')
 
 // Configure dotenv
 dotenv.config({ path: './config/config.env' })
+connectDB()
 
 const app = express()
 
-connectDB()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Routes
+app.use('/testimonies', require('./routes/public'))
+
 
 const PORT = process.env.PORT || 3000
 
