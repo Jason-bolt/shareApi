40593fe59dbd86245b56ce43a28db525514b2829
Jason-bolt <appiatujason@gmail.com>
2022-12-02 14:17:03 +0000

    Passing name of user with testimnies

--- routes/public.js
+++ routes/public.js
@@ -1,15 +1,13 @@
 const router = require('express').Router()
 const Testimony = require('../models/Testimony')
-var _ = require('lodash');
 
 // @desc    Get all testimonies
 // @route   FET /testimonies
 router.get('/', async (req, res) => {
 
     try {
-        const testimonies = await Testimony.find({}).populate('user').lean()
-        res.status(200).send(_.omit(JSON.parse(JSON.stringify(testimonies)), ['user.password']))
-        // res.status(200).send(testimonies)
+        const testimonies = await Testimony.find({}).populate('user', 'name').lean()
+        res.status(200).send(testimonies)
     } catch (err) {
         console.error(err)
         res.status(500).send('Sorry, something went wrong!')
