01256b8b78f0170a885a581bad04963be01cc490
Jason-bolt <appiatujason@gmail.com>
2022-12-02 20:18:27 +0000

    Created controllers

--- routes/auth.js
+++ routes/auth.js
@@ -1,65 +1,9 @@
 const router = require('express').Router()
-const Testimony = require('../models/Testimony')
-const bcrypt = require('bcrypt')
-const User = require('../models/User')
+const UsersController = require('../controllers/UsersController')
 
 // @desc    Register user
 // @route   PUT /register
-router.post('/register', async (req, res) => {
-
-    const { email, name, password, confirm_password } = req.body
-    const errors = []
-    // Validating input
-    if (!email){
-        errors.push('Email is required!')
-    }
-
-    if(!name){
-        errors.push('Name is required!')
-    }
-    
-    if(!password){
-        errors.push('Password is required!')
-    }
-    
-    if(!confirm_password){
-        errors.push('Password confirmation is required!')
-    }
-    
-    if(password !== confirm_password){
-        errors.push('Passwords do not match is required!')
-    }
-
-    if (errors.length > 0){
-        res.status(403).send(errors)
-    }else{
-        try {
-            const user = await User.findOne({ email: email })
-    
-            if (user){
-                res.status(409).send("User already exists")
-            }else{
-                hashed_password = await bcrypt.hash(req.body.password, 10)
-                
-                const newUser = {
-                    email: req.body.email,
-                    name: req.body.name,
-                    password: hashed_password
-                }
-                // await User.create()
-    
-                res.status(201).send(newUser)
-            }
-    
-        } catch (err) {
-            res.status(500).send("Server error!")        
-        }
-    }
-
-
-
-
-})
+router.post('/register', [UsersController.create])
 
 
 module.exports = router
