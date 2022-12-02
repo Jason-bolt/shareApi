9eefd92012923e57ce86882792af6756de4d321d
Jason-bolt <appiatujason@gmail.com>
2022-12-02 11:29:33 +0000

    Added database

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const path = require('path')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
+// Configure dotenv
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
+
+connectDB()
 
 const PORT = process.env.PORT || 3000
 
